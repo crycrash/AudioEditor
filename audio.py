e434294b1960a2cb4d63008a0a26b03c4902243e
milana <monstermusic.club@yandex.ru>
2024-03-28 21:32:55 +0500

    Работа через консоль. Функция вставки аудио. Функция ускорения/замедления аудио

--- audio.py
+++ audio.py
@@ -3,12 +3,6 @@ import struct
 
 
 class Audiofile:
-    stHeaderFields = {'ChunkSize': 0, 'Format': '',
-                      'Subchunk1Size': 0, 'AudioFormat': 0,
-                      'NumChannels': 0, 'SampleRate': 0,
-                      'ByteRate': 0, 'BlockAlign': 0,
-                      'BitsPerSample': 0, 'Filename': '',
-                      'Subchunk2Id': 0, 'SizeSec': 0}
 
     def __init__(self, path):
         self.path = path
@@ -17,9 +11,23 @@ class Audiofile:
             wav_in.seek(44, os.SEEK_CUR)
 
             self.audio_data = wav_in.read()
+            self.stHeaderFields = {'ChunkSize': 0, 'Format': '',
+                                   'Subchunk1Size': 0, 'AudioFormat': 0,
+                                   'NumChannels': 0, 'SampleRate': 0,
+                                   'ByteRate': 0, 'BlockAlign': 0,
+                                   'BitsPerSample': 0, 'Filename': '',
+                                   'Subchunk2Id': 0, 'SizeSec': 0}
             if (self.header[0:4] != b'RIFF') or \
                     (self.header[12:16] != b'fmt '):
                 raise Exception("Not valid")
+
+    def output_in_file(self):
+        f = open(self.path, 'w+')
+        f.seek(0)
+        f.close()
+        with open(self.path, 'wb') as wav_out:
+            wav_out.write(self.header)
+            wav_out.write(self.audio_data)
 
     def take_header_config(self):
         """Парсинг заголовка wav и высчитывание длины аудио в секундах"""
@@ -35,7 +43,7 @@ class Audiofile:
         self.stHeaderFields['Subchunk2Id'] = struct.unpack('<L', self.header[36:40])[0]
         self.stHeaderFields['Subchunk2Size'] = struct.unpack('<L', self.header[40:44])[0]
         self.stHeaderFields['SizeSec'] = self.stHeaderFields['Subchunk2Size'] / (
-                    self.stHeaderFields['BitsPerSample'] / 8) / self.stHeaderFields['SampleRate'] / self.stHeaderFields[
+                self.stHeaderFields['BitsPerSample'] / 8) / self.stHeaderFields['SampleRate'] / self.stHeaderFields[
                                              'NumChannels']
         print(self.stHeaderFields['SizeSec'])
 
@@ -44,16 +52,43 @@ class Audiofile:
         if start_point > self.stHeaderFields['SizeSec'] or end_point > self.stHeaderFields['SizeSec']:
             raise Exception('You have gone beyond the allowed length')
 
-        count = len(self.audio_data)//self.stHeaderFields['SizeSec']
-        self.audio_data = self.audio_data[int(start_point*count):int(end_point*count)]
-        self.stHeaderFields['Subchunk2Size'] = self.stHeaderFields['Subchunk2Size']//2
+        count = len(self.audio_data) // self.stHeaderFields['SizeSec']
+        self.audio_data = self.audio_data[int(start_point * count):int(end_point * count)]
+        self.stHeaderFields['Subchunk2Size'] = self.stHeaderFields['Subchunk2Size'] // 2
         self.stHeaderFields['ChunkSize'] = self.stHeaderFields['Subchunk2Size'] + 36
         self.header = self.header.replace(self.header[40:44], struct.pack('<L', self.stHeaderFields['Subchunk2Size']))
         self.header = self.header.replace(self.header[4:8], struct.pack('<L', self.stHeaderFields['ChunkSize']))
-        self.stHeaderFields['SizeSec'] = self.stHeaderFields['SizeSec']//2
-        f = open(self.path, 'w+')
-        f.seek(0)
-        f.close()
-        with open(self.path, 'wb') as wav_out:
-            wav_out.write(self.header)
-            wav_out.write(self.audio_data)
+        self.stHeaderFields['SizeSec'] = self.stHeaderFields['SizeSec'] // 2
+        self.output_in_file()
+
+    def splice_file(self, other, time):
+        """Вставка одного файла в другой"""
+        if time > int(self.stHeaderFields['SizeSec']):
+            raise Exception('You have gone beyond the allowed length')
+        if isinstance(other, Audiofile):
+            count = len(self.audio_data) // self.stHeaderFields['SizeSec']
+            self.audio_data = (self.audio_data[:int(time * count)] +
+                               other.audio_data
+                               + self.audio_data[int(time * count):])
+            self.stHeaderFields['Subchunk2Size'] = (self.stHeaderFields['Subchunk2Size']
+                                                    + other.stHeaderFields['Subchunk2Size'])
+            self.stHeaderFields['ChunkSize'] = self.stHeaderFields['Subchunk2Size'] + 36
+            self.header = self.header.replace(self.header[40:44],
+                                              struct.pack('<L', self.stHeaderFields['Subchunk2Size']))
+            self.header = self.header.replace(self.header[4:8],
+                                              struct.pack('<L', self.stHeaderFields['ChunkSize']))
+            self.stHeaderFields['SizeSec'] = self.stHeaderFields['SizeSec'] + other.stHeaderFields['SizeSec']
+            self.output_in_file()
+        else:
+            raise Exception('Что то пошло не так')
+
+    def speed_multiplying(self, speed):
+        """Ускорение или замедление аудио дорожки"""
+        self.stHeaderFields['SampleRate'] = int(self.stHeaderFields['SampleRate']*speed)
+        self.stHeaderFields['Subchunk2Size'] = int(self.stHeaderFields['Subchunk2Size'] * 1/speed)
+        self.stHeaderFields['ChunkSize'] = self.stHeaderFields['Subchunk2Size'] + 36
+        self.header = self.header.replace(self.header[40:44], struct.pack('<L', self.stHeaderFields['Subchunk2Size']))
+        self.header = self.header.replace(self.header[4:8], struct.pack('<L', self.stHeaderFields['ChunkSize']))
+        self.header = self.header.replace(self.header[24:28], struct.pack('<L', self.stHeaderFields['SampleRate']))
+        self.stHeaderFields['SizeSec'] = self.stHeaderFields['SizeSec'] * 1/speed
+        self.output_in_file()
--- main.py
+++ main.py
@@ -1,14 +1,45 @@
-import sys
+import argparse
+
 from audio import Audiofile
 
 
 def main():
-    if len(sys.argv) == 3:
-        if sys.argv[2] == "save":
-            a = Audiofile(sys.argv[1])
-            a.take_header_config()
-            a.crop_file(1, 2)
-            print('ALL right')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('command', choices=['save', 'erase', 'splice', 'speed', 'stop'], help='Команда')
+
+    example = None
+    while True:
+        command = input("Введите команду (save, read, erase, splice, speed): ")
+        if command == 'save':
+            filename = input("Введите название файла для сохранения: ")
+            example = Audiofile(filename)
+            example.take_header_config()
+        elif command == 'erase':
+            start, finish = [int(x) for x in input("Введите координаты обрезки ").split()]
+            if example is not None:
+                example.crop_file(start, finish)
+            else:
+                raise Exception("Произведите сохранение файла")
+        elif command == 'splice':
+            filename = input("Введите название файла с которым будет склейка: ")
+            time = input("Введите время в которое хотите вставить :* ")
+            other = Audiofile(filename)
+            other.take_header_config()
+            if other is not None and example is not None:
+                example.splice_file(other, int(time))
+            else:
+                raise Exception("Произведите сохранение файла")
+        elif command == 'speed':
+            speed = input('Введите коэфициент для замедления/ускорения аудио')
+            if example is not None:
+                example.speed_multiplying(float(speed))
+            else:
+                raise Exception("Произведите сохранение файла")
+        elif command == 'stop':
+            print("Программа завершена")
+            break
+        else:
+            print("Некорректная команда")
 
 
 if __name__ == "__main__":
